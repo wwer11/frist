0f9ae6a6fd800a89a612de412918a981bb421b70
wwer11 <sakibmiah9876@gmail.com>
2022-11-15 12:01:45 +0600

    sakib

---
+++ index.js
@@ -0,0 +1,197 @@
+// console.log("hello word")
+
+// var myName = ("Sakib")
+// console.log(myName)
+
+// var strType =('only letter is string')
+// console.log(typeof strType)
+
+
+// var numType =87668
+// console.log(numType)
+
+// console.log(typeof numType)
+
+
+
+
+
+
+
+// var boolType = true;
+// console.log(boolType)
+
+// var boolType = false;
+// console.log(boolType)
+
+
+
+// var rocona ="hi Everyone how are you eve";
+// console.log(rocona.toUpperCase())
+
+// console.log(rocona.toLowerCase())
+
+
+// var myfd ="sakib  sa  ki";
+// console.log(myfd.split('sakib'));
+
+
+
+// console.log(myfd.indexOf('i'));
+
+
+
+// var sonka = 765;
+// var sonka = 766;
+// var mot = sonka + sonka
+// console.log(mot);
+
+
+
+
+// var sonka = 765.977;
+// var sonka = 766;
+
+// sonka = parseInt(sonka);
+
+// var mot = sonka + sonka
+// console.log(mot);
+
+
+
+
+
+// var sonka1 = 765.3477;
+// var sonka = 766;
+
+// sonka1 = parseFloat(sonka1);
+
+// var mot = sonka1 + sonka
+// console.log(mot);
+
+
+
+
+
+// var name1 = 29;
+// var name2 = 20;
+
+// name1 = "" + name1;
+
+
+// console.log(typeof name1);
+
+// var total = name1 + name2
+
+// console.log(total);
+
+
+
+
+
+
+
+// var num1 = 40;
+// var num2 = 404;
+// var total = num1 - num2
+// console.log(total);
+
+
+
+// var sonka1 = 59;
+// var sonka = 50;
+
+// var total = sonka * sonka1
+// console.log(total);
+
+
+
+// var sonka1 = 50;
+// var sonka2 = 5;
+// console.log(sonka1 / sonka2);
+
+
+// var number1 = 80;
+// var number2 = 7;
+
+// console.log(number1 % number2);
+
+
+// var num1 = -90;
+// var mot = Math.abs(num1)
+// console.log(mot);
+
+
+   
+
+
+
+
+
+
+// var number1 = 80.6;
+// var number2 = 7.4;
+// var mot = Math.round(number1)
+// console.log(mot);
+// var mot1 = Math.round(number2)
+// console.log(mot1);
+
+
+
+
+
+
+
+
+// var number1 = 80.6;
+// var number2 = 7.1;
+// var mot = Math.ceil(number1)
+// console.log(mot);
+
+// var mot1 = Math.ceil(number2)
+// console.log(mot1);
+
+
+
+
+
+
+
+// var number1 = 80.9999;
+// var number2 = 7.1111;
+// var mot = Math.floor(number1)
+// console.log(mot);
+
+// var mot1 = Math.floor(number2)
+// console.log(mot1);
+
+
+
+
+
+// var number1 = 80.6;
+// var number2 = 7.1;
+// var mot = Math.random(number1) * 500;
+// console.log(mot);
+
+
+
+
+
+
+
+
+
+var a = 10;
+var b = 1;
+
+// if(a > b){
+//     console.log("see you not for mind")
+// }
+if(a < b){
+    console.log("see you not for mind")
+}
+
+
+
+
